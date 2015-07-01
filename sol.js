4979cb77bb93a3152b39146f2c5a073b82d83e10
unknown <ybai@ybaipc.pc.factset.com>
2015-07-01 18:05:30 -0400

    s

--- sol.js
+++ sol.js
@@ -1,3 +1,32 @@
+// 20
+var isValid = function(s) {
+    if (s.length % 2 !== 0) {
+        return false;
+    }
+    
+    var parenStack = [],
+    parenMap = {
+        '(' : ')',
+        '{' : '}',
+        '[' : ']'
+    },
+    leftParen = Object.keys(parenMap);
+    
+    for (var i = 0; i < s.length; i++) {
+        if (leftParen.indexOf(s.charAt(i)) > -1) {
+            parenStack.push(s.charAt(i));
+        } else {
+            if (!parenStack.length) {
+                return false;
+            }
+            if (parenMap[parenStack.pop()] !== s.charAt(i)) {
+                return false;
+            }
+        }
+    }
+    return (parenStack.length) ? false : true;
+};
+
 // 136
 var singleNumber = function(nums) {
     var singleNum = 0;
@@ -9,8 +38,9 @@ var singleNumber = function(nums) {
 
 // 206
 var reverseList = function(head) {
-    var temp = null; next = null;
-    while(head !== null) {
+    var temp = null,
+        next = null;
+    while (head !== null) {
         next = head.next;
         head.next = temp;
         temp = head;
