e7875c4a397ac07a3c58b07d0ccf7d637598d38c
Yang Bai <yccovin@gmail.com>
2015-12-07 01:06:55 -0500

    s

--- lc.js
+++ lc.js
@@ -1,11 +1,37 @@
 // 1
 let twoSum = (nums, target) => {
-  let numMap = new Map();
-  for (let i = 0; i < nums.length; i++) {
-    if (numMap.has(target - nums[i])) return [numMap.get(target - nums[i]) + 1, i + 1];
-    numMap.set(nums[i], i);
-  }
+    let numMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+      if (numMap.has(target - nums[i])) return [numMap.get(target - nums[i]) + 1, i + 1];
+      numMap.set(nums[i], i);
+    }
 };
+
+// 2
+let addTwoNumbers = (l1, l2) => {
+    let carryOver = 0;
+    let head = new ListNode(0);
+    let tmp = head;
+
+    while (l1 || l2) {
+      let sum = 0;
+      if (l1 && l2) sum = l1.val + l2.val + carryOver;
+      else if (l1) sum += l1.val + carryOver;
+      else sum += l2.val + carryOver;
+
+      if (sum > 9) {
+        carryOver = 1;
+        sum -= 10;
+      } else carryOver = 0;
+
+      tmp.next = new ListNode(sum);
+      tmp = tmp.next;
+      l1 = l1 ? l1.next : l1;
+      l2 = l2 ? l2.next : l2;
+    }
+    if (carryOver) tmp.next = new ListNode(carryOver);
+    return head.next;
+}
 
 // 13
 let romanToInt = s => {
@@ -30,6 +56,27 @@ let romanToInt = s => {
         }
     }
     return total;
+};
+
+// 19
+let removeNthFromEnd = (head, n) => {
+    let tmp = new ListNode(0);
+    let tmpHead = head;
+    tmp.next = head;
+    
+    for (let i = 0; i < n; i++) {
+        head = head.next;
+    }
+    
+    if (!head) return tmp.next.next;
+    
+    while (head) {
+        tmp = tmp.next;
+        head = head.next;
+    }
+    
+    tmp.next = tmp.next.next;
+    return tmpHead;
 };
 
 // 20
@@ -110,6 +157,12 @@ let removeElement = (nums, val) => {
     return nums.length;
 };
 
+// 58
+let lengthOfLastWord = s => {
+    let words = s.trim().split(' ');
+    return words[words.length - 1].length;
+};
+
 // 70
 let climbStairs = n => {
     let fibCache = 1;
@@ -169,8 +222,24 @@ let levelOrder = root => {
 
 // 104
 let maxDepth = root => {
-    if (root === null) return 0;
+    if (!root) return 0;
     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
+
+// 110
+let isBalanced = root => {
+    let getHeight = root => {
+      if (!root) return 0;
+      return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+    }
+
+    if (!root) return true;
+    
+    let leftHeight = getHeight(root.left);
+    let rightHeight = getHeight(root.right);
+    
+    if (Math.abs(leftHeight - rightHeight) > 1) return false;
+    return isBalanced(root.left) && isBalanced(root.right);
 };
 
 // 111
@@ -186,132 +255,186 @@ let minDepth = root => {
 
 // 112
 let hasPathSum = (root, sum) => {
-  let preOrder = (root, runningSum) => {
+    let preOrder = (root, runningSum) => {
+      if (!root) return false;
+      if (!root.left && !root.right) return (runningSum + root.val) === sum;
+      return (preOrder(root.left, runningSum + root.val) || 
+          (preOrder(root.right, runningSum + root.val)));
+    }
+    
     if (!root) return false;
-    if (!root.left && !root.right) return (runningSum + root.val) === sum;
-    return (preOrder(root.left, runningSum + root.val) || 
-        (preOrder(root.right, runningSum + root.val)));
-  }
-  
-  if (!root) return false;
-  return preOrder(root, 0);
+    return preOrder(root, 0);
 };
 
 // 118
 let generate = numRows => {
-  let result = [[1], [1, 1]];
+    let result = [[1], [1, 1]];
 
-  if (numRows === 0) return [];
-  if (numRows < 3) return result.slice(0, numRows);
+    if (numRows === 0) return [];
+    if (numRows < 3) return result.slice(0, numRows);
 
-  while (numRows > result.length) {
-    let prevArray = result[result.length - 1];
-    let nextRow = [1];
-    for (let i = 1; i < result.length; i++) {
-      nextRow.push(prevArray[i - 1] + prevArray[i]);
+    while (numRows > result.length) {
+      let prevArray = result[result.length - 1];
+      let nextRow = [1];
+      for (let i = 1; i < result.length; i++) {
+        nextRow.push(prevArray[i - 1] + prevArray[i]);
+      }
+      nextRow.push(1);
+      result.push(nextRow);
     }
-    nextRow.push(1);
-    result.push(nextRow);
-  }
-  return result;
+    return result;
 };
 
 // 119
 let getRow = rowIndex => {
-  let pascalCache = [[1], [1, 1]];
-  if (rowIndex <= 1) {
-    return pascalCache[rowIndex];
-  }
-  let prev = pascalCache[1];
-  let result = [];
-  
-  while (rowIndex > 1) {
-    result = [1];
-    for (let i = 1; i < prev.length; i++) {
-      result.push(prev[i - 1] + prev[i]);
+    let pascalCache = [[1], [1, 1]];
+    if (rowIndex <= 1) {
+      return pascalCache[rowIndex];
     }
-    result.push(1);
-    prev = result;
-    rowIndex--;
-  }
-  return result;
+    let prev = pascalCache[1];
+    let result = [];
+    
+    while (rowIndex > 1) {
+      result = [1];
+      for (let i = 1; i < prev.length; i++) {
+        result.push(prev[i - 1] + prev[i]);
+      }
+      result.push(1);
+      prev = result;
+      rowIndex--;
+    }
+    return result;
 };
+
+// 121
+let maxProfit = prices => {
+    let profit = 0;
+    let minPrice = prices[0];
+    for (let i = 1; i < prices.length; i++) {
+        let potentialProfit = prices[i] - minPrice;
+        profit = (potentialProfit > profit) ? potentialProfit : profit;
+        if (minPrice > prices[i]) minPrice = prices[i]; 
+    }
+    return profit;
+};
+
+// 122
+let maxProfit = prices => {
+  let profit = 0;
+
+  if (!prices.length) return 0;
+  let currentPrice = prices[0];
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] > currentPrice) {
+      profit += (prices[i] - currentPrice);
+      currentPrice = prices[i];
+    }
+    if (currentPrice > prices[i]) currentPrice = prices[i];
+  }
+  return profit;
+}
 
 // 125
 let isPalindrome = s => {
-  s = s.replace(/\W/g, '').toUpperCase();
-  
-  for (let i = 0, j = s.length - 1; i < s.length / 2; i++, j--) {
-    if (s[i] !== s[j]) return false;
-  }
-  return true;
+    s = s.replace(/\W/g, '').toUpperCase();
+    
+    for (let i = 0, j = s.length - 1; i < s.length / 2; i++, j--) {
+      if (s[i] !== s[j]) return false;
+    }
+    return true;
+};
+
+// 136
+let singleNumber = nums => {
+    return nums.reduce((single, num) => single ^ num);  
+};
+
+// 144
+let preorderTraversal = root => {
+    if (!root) return [];
+    let stack = [root];
+    let preOrder = [];
+    
+    while (stack.length) {
+        let node = stack.pop();
+        preOrder.push(node.val);
+        if (node.right) stack.push(node.right);
+        if (node.left) stack.push(node.left);
+    }
+    return preOrder;
 };
 
 // 155
 class MinStack {
-  constructor() {
-    this.stack = [];
-    this.minStack = [];
-  }
-  push(x) {
-    let lastIdx = this.stack.length - 1;
-    if (this.minStack[lastIdx] > x || this.minStack[lastIdx] === undefined) this.minStack.push(x);
-    else this.minStack.push(this.minStack[lastIdx]);
-    this.stack.push(x);
-  }
-  pop() {
-    // should throw error if length === 0, but leetcode doesn't require it.
-    this.stack.pop();
-    this.minStack.pop();
-  }
-  top() {
-    return this.stack[this.stack.length - 1];
-  }
-  getMin() {
-    return this.minStack[this.stack.length - 1];
-  }
+    constructor() {
+      this.stack = [];
+      this.minStack = [];
+    }
+    push(x) {
+      let lastIdx = this.stack.length - 1;
+      if (this.minStack[lastIdx] > x || this.minStack[lastIdx] === undefined) this.minStack.push(x);
+      else this.minStack.push(this.minStack[lastIdx]);
+      this.stack.push(x);
+    }
+    pop() {
+      // should throw error if length === 0, but leetcode doesn't require it.
+      this.stack.pop();
+      this.minStack.pop();
+    }
+    top() {
+      return this.stack[this.stack.length - 1];
+    }
+    getMin() {
+      return this.minStack[this.stack.length - 1];
+    }
 }
 
 // 169
 let majorityElement = nums => {
-  // moore's voting algorithm
-  let counter = 1;
-  return nums.reduce((majority, num, index) => {
-    if (num === majority) counter++;
-    else {
-      if (counter !== 0) counter--;
-      else majority = num;
-    }
-    return majority;
-  });
+    // moore's voting algorithm
+    let counter = 1;
+    return nums.reduce((majority, num, index) => {
+      if (num === majority) counter++;
+      else {
+        if (counter !== 0) counter--;
+        else majority = num;
+      }
+      return majority;
+    });
 };
 
 // 171
 let titleToNumber = s => {
-  let strCode = 0;
-  let index = s.length - 1;
-  
-  for (let ch of s) {
-    strCode += (ch.charCodeAt(0) - 64) * Math.pow(26, index--); 
-  }
-  return strCode;
+    let strCode = 0;
+    let index = s.length - 1;
+    
+    for (let ch of s) {
+      strCode += (ch.charCodeAt(0) - 64) * Math.pow(26, index--); 
+    }
+    return strCode;
+};
+
+// 198
+let rob = nums => {
+  return nums.reduce((memo, num) => [memo[1], Math.max(num + memo[0], memo[1])], [0, 0])[1]
 };
 
 // 203
 let removeElements = (head, val) => {
-  if (!head) return null;
-  
-  let ptr = new ListNode(0);
-  let tmpHead = ptr;
-  
-  while (head) {
-    if (head.val !== val) {
-      ptr.next = new ListNode(head.val);
-      ptr = ptr.next;
-    }  
-    head = head.next;
-  }
-  return tmpHead.next;
+    if (!head) return null;
+    
+    let ptr = new ListNode(0);
+    let tmpHead = ptr;
+    
+    while (head) {
+      if (head.val !== val) {
+        ptr.next = new ListNode(head.val);
+        ptr = ptr.next;
+      }  
+      head = head.next;
+    }
+    return tmpHead.next;
 };
 
 // 206
@@ -338,14 +461,14 @@ let containsDuplicate = nums => {
 
 // 219
 let containsNearbyDuplicate = (nums, k) => {
-  let numMap = new Map();
-  
-  for (let i = 0; i < nums.length; i++) {
-    let num = nums[i];
-    if (numMap.has(num) && i - numMap.get(num) <= k) return true;
-    else numMap.set(num, i);
-  }
-  return false;
+    let numMap = new Map();
+    
+    for (let i = 0; i < nums.length; i++) {
+      let num = nums[i];
+      if (numMap.has(num) && i - numMap.get(num) <= k) return true;
+      else numMap.set(num, i);
+    }
+    return false;
 };
 
 // 226
@@ -363,50 +486,59 @@ let invertTree = root => {
     return head;
 };
 
+// 231
+let isPowerOfTwo = n => {
+    if (n < 1) return false;
+    while (n % 2 === 0) {
+        n = n / 2;
+    }
+    return n === 1;
+};
+
 // 232
 class Queue {
-  constructor() {
-    this.stack = [];
-    this.secStack = [];
-  }
-  push(x) {
-    this.secStack = [];
-    this.stack.push(x);
-    let tmp = this.stack.slice();
-    while (this.stack.length) {
-      this.secStack.push(this.stack.pop());
+    constructor() {
+      this.stack = [];
+      this.secStack = [];
     }
-    this.stack = tmp;
-  }
-  pop() {
-    this.stack.pop();
-    return this.secStack.pop();
-  }
-  peek() {
-    return this.secStack[this.stack.length - 1];
-  }
-  empty() {
-    return this.stack.length === 0;
-  }
+    push(x) {
+      this.secStack = [];
+      this.stack.push(x);
+      let tmp = this.stack.slice();
+      while (this.stack.length) {
+        this.secStack.push(this.stack.pop());
+      }
+      this.stack = tmp;
+    }
+    pop() {
+      this.stack.pop();
+      return this.secStack.pop();
+    }
+    peek() {
+      return this.secStack[this.stack.length - 1];
+    }
+    empty() {
+      return this.stack.length === 0;
+    }
 }
 
 // 235
 let lowestCommonAncestor = (root, p, q) => {
-  if (!root) return null;
-  let smallerRoot = (p.val > q.val) ? q : p;
-  let largerRoot = (p.val > q.val) ? p : q;
-  
-  let preOrder = (root, smaller, larger) => {
-    if (!root) return;
-    if ((root.val === smaller.val) || (root.val === larger.val)) return root;
-    if ((root.val > smaller.val) && (root.val < larger.val)) return root;
-    if (root.val > larger.val) {
-      return preOrder(root.left, smallerRoot, largerRoot);
-    } else {
-      return preOrder(root.right, smallerRoot, largerRoot);
+    if (!root) return null;
+    let smallerRoot = (p.val > q.val) ? q : p;
+    let largerRoot = (p.val > q.val) ? p : q;
+    
+    let preOrder = (root, smaller, larger) => {
+      if (!root) return;
+      if ((root.val === smaller.val) || (root.val === larger.val)) return root;
+      if ((root.val > smaller.val) && (root.val < larger.val)) return root;
+      if (root.val > larger.val) {
+        return preOrder(root.left, smallerRoot, largerRoot);
+      } else {
+        return preOrder(root.right, smallerRoot, largerRoot);
+      }
     }
-  }
-  return preOrder(root, smallerRoot, largerRoot);
+    return preOrder(root, smallerRoot, largerRoot);
 };
 
 // 237
@@ -443,16 +575,16 @@ let isAnagram = (s, t) => {
 
 // 257
 let binaryTreePaths = root => {
-  let paths = [];
-  let preOrder = (root, path) => {
-    if (!root) return;
-    path = path.concat(root.val);
-    if (!root.left && !root.right) paths.push(path.join('->'));  
-    preOrder(root.left, path);
-    preOrder(root.right, path);
-  }
-  preOrder(root, []);
-  return paths;
+    let paths = [];
+    let preOrder = (root, path) => {
+      if (!root) return;
+      path = path.concat(root.val);
+      if (!root.left && !root.right) paths.push(path.join('->'));  
+      preOrder(root.left, path);
+      preOrder(root.right, path);
+    }
+    preOrder(root, []);
+    return paths;
 };
 
 // 258 
@@ -479,25 +611,25 @@ let moveZeroes = nums => {
 
 // 290
 let wordPattern = (pattern, str) => {
-  let charMap = new Map();
-  let wordMap = new Map();
-  let words = str.split(' ');
-  let idx = 0;
-  
-  if (pattern.length !== words.length) return false;
-  
-  for (ch of pattern) {
-    if (charMap.has(ch)) {
-      if (charMap.get(ch) !== words[idx]) return false;
-    } else charMap.set(ch, words[idx]);
+    let charMap = new Map();
+    let wordMap = new Map();
+    let words = str.split(' ');
+    let idx = 0;
     
-    if (wordMap.has(words[idx])) {
-      if (wordMap.get(words[idx]) !== ch) return false;  
-    } else wordMap.set(words[idx], ch);
+    if (pattern.length !== words.length) return false;
     
-    idx++;
-  }
-  return true;
+    for (ch of pattern) {
+      if (charMap.has(ch)) {
+        if (charMap.get(ch) !== words[idx]) return false;
+      } else charMap.set(ch, words[idx]);
+      
+      if (wordMap.has(words[idx])) {
+        if (wordMap.get(words[idx]) !== ch) return false;  
+      } else wordMap.set(words[idx], ch);
+      
+      idx++;
+    }
+    return true;
 };
 
 // 292
